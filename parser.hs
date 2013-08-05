62c7df9a2b556508e6f9286b4fdb6cfdd5a0e669
zrathustra <ahr12012@mymail.pomona.edu>
2013-08-05 08:34:52 -0700

    basic parser implemented, working on understanding monads

---
+++ parser.hs
@@ -0,0 +1,60 @@
+import Text.ParserCombinators.Parsec hiding (spaces)
+import System.Environment
+import Control.Monad
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+
+main :: IO ()
+main = do 
+    args <- getArgs
+    putStrLn (readExpr (args !! 0))
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+readExpr :: String -> String
+readExpr input = case parse parseExpr "lisp" input of
+    Left err -> "No match: " ++ show err
+    Right val -> "Found value"
+
+parseString :: Parser LispVal
+parseString = do
+                char '"'
+                x <- many parseEscaped
+                char '"'
+                return $ String $ foldr (++) [] x
+
+parseEscaped :: Parser Char
+parseEscaped = do
+            x <- string "\\n"
+            <|> string "\\\""
+            <|> string "\\r"
+            <|> string "\\t"
+            <|> string "\\"
+            <|> liftM $ noneOf "\""            
+
+parseAtom :: Parser LispVal
+parseAtom = do 
+                first <- letter <|> symbol
+                rest <- many (letter <|> digit <|> symbol)
+                let atom = first:rest
+                return $ case atom of
+                            "#t" -> Bool True
+                            "#f" -> Bool False
+                            _    -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+        <|> parseString
+        <|> parseNumber
